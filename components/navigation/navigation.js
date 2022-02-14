8df6cf3d2d3f009a1267d420d9e89d1b4884293d
Anton Nell <antonnell89@gmail.com>
2022-02-14 10:37:55 +0200

    Version 0.7.6 Updated to latest AMM contract
    Signed-off-by: Anton Nell <antonnell89@gmail.com>

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -542,7 +542,7 @@ function Navigation(props) {
           </Grid>
         </Grid>
       </div>
-      <Typography className={classes.smallVersion}>Version 0.7.5</Typography>
+      <Typography className={classes.smallVersion}>Version 0.7.6</Typography>
       { warningOpen &&
         <FFWarning close={ closeWarning } />
       }
